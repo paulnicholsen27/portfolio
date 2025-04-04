62840d603799bd8d46fd0a53403737323886dc47
Paul Nicholsen <paulnicholsen27@gmail.com>
2025-04-04 12:00:30 -0400

    chunked resume

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .ai_chatbot import query_chatbot
+
+def chat_api(request):
+    if request.method == 'POST':
+        question = json.loads(request.body).get('message', '')
+        response = query_chatbot(question)
+        return JsonResponse({'response': response})
