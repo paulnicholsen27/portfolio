6d83d2ea21326ad9ebb5fc51273e8cf586fb40ac
Paul Nicholsen <paulnicholsen27@gmail.com>
2025-04-17 19:25:12 -0400

    deleted main app

--- main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Resume(models.Model):
-    content = models.TextField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Resume uploaded on {self.uploaded_at}"
