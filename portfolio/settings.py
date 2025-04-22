0210be2cff36f2292dc865fae7831af41f2cb6ad
Paul Nicholsen <paulnicholsen27@gmail.com>
2025-04-22 10:56:47 -0400

    more settings crap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://paulnicholsenportfolio.netlify.app",
     "https://paulnicholsen.com",
 ]
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
