51817da113ee3d2a7a841336a9ca713164199bae
Paul Nicholsen <paulnicholsen27@gmail.com>
2025-06-27 16:46:05 -0400

    test deploy

--- reactfolio/src/index.js
+++ reactfolio/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+console.log("🔥 Rebuild happened");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
